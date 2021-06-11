04a9284b7a7477e48478fde6a2a0aa2435f7f320
Matt <mattwebbersemail@gmail.com>
2021-06-11 16:37:43 -0400

    fixes bug with link

--- client/src/components/Contacts/ContactItem.js
+++ client/src/components/Contacts/ContactItem.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export const ContactItem = ({contact}) => {
-  const {lastName, firstName, url} = contact
+  const {lastName, firstName, doxyUrl} = contact
   return (
     <li className='collection-item'>
       <span className='title'>
         {lastName}, {firstName} -{' '}
       </span>
-      <a href={url} rel='noreferrer' target='_blank'>
+      <a href={doxyUrl} rel='noreferrer' target='_blank'>
         Link
       </a>
     </li>
