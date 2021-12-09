35dde1f2ec464add9c7768ee002cf0b4107844db
Matt @ Work <mattwebbersemail@gmail.com>
2021-12-09 11:31:43 -0500

    regular doxy directory

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = jsonServer.create()
 const express = require('express')
 const path = require('path')
 const middlewares = jsonServer.defaults({static: './client/build'})
-const router = jsonServer.router('all_db.json')
+const router = jsonServer.router('new_db.json')
 const PORT = process.env.PORT || 5000
 
 app.use(middlewares)
