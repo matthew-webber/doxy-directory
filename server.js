737fe4a07b81f31dfdce329cd7f8d63cddbabffa
Matt Webber (personal iMac) <matthew.lee.webber@gmail.com>
2022-12-06 13:29:38 -0500

    testing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 const app = jsonServer.create()
 const express = require('express')
 const path = require('path')
-const middlewares = jsonServer.defaults({static: './client/build'})
+const middlewares = jsonServer.defaults({static: './client'})
 const router = jsonServer.router('new_db.json')
 const PORT = process.env.PORT || 5000
 
@@ -10,16 +10,22 @@ app.use(middlewares)
 app.use(router)
 app.use(express.static(path.join(__dirname, 'build')))
 
+console.log('path.join(__dirname, build, index.html):', path.join(__dirname, 'client', 'build', 'index.html') )
 app.get('/*', function (req, res) {
+  console.log('hi')
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-if (process.env.NODE_ENV === 'production') {
+if (true) {
+// if (process.env.NODE_ENV === 'production') {
+  console.log('heya')
   app.use(express.static('client/build'))
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve('client', 'build', 'index.html'))
+    {res.sendFile(path.resolve('client', 'build', 'index.html'));
+    console.log('hiya')}
   )
 } else {
+  console.log('hello')
 }
 
 app.listen(PORT)
