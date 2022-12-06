ebd03dc14a79f2962abea77fd5b046fda65a0379
Matt Webber (personal iMac) <matthew.lee.webber@gmail.com>
2022-12-06 16:04:08 -0500

    testing

--- server.js
+++ server.js
@@ -1,33 +1,28 @@
 const jsonServer = require('json-server')
-const app = jsonServer.create()
-const express = require('express')
+const server = jsonServer.create()
 const path = require('path')
 const middlewares = jsonServer.defaults({static: './client'})
-const router = jsonServer.router('new_db.json')
-const PORT = process.env.PORT || 5000
+const router = jsonServer.router(path.join(__dirname, 'new_db.json'))
+const port = process.env.PORT || 10000
 
-app.use(middlewares)
-app.use(router)
-app.use(express.static(path.join(__dirname, 'build')))
+server.use(middlewares)
+server.use(router)
+// app.use(express.static(path.join(__dirname, 'client', 'build')))
 
-console.log('path.join(__dirname, build, index.html):', path.join(__dirname, 'client', 'build', 'index.html') )
-app.get('/*', function (req, res) {
-  console.log('hi')
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+// app.get('/*', function (req, res) {
+//   console.log('hi')
+//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+// })
 
-if (true) {
+// if (true) {
 // if (process.env.NODE_ENV === 'production') {
-  console.log('heya')
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) =>
-    {res.sendFile(path.resolve('client', 'build', 'index.html'));
-    console.log('hiya')}
-  )
-} else {
-  console.log('hello')
-}
+//   console.log(path.resolve('client', 'build', 'index.html'), 'hello')
+//   app.use(express.static('client/build'))
+//   app.get('*', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
+// } else {
+//   console.log('hello')
+// }
 
-app.listen(PORT)
-
-console.log('Server started on PORT', PORT)
+server.listen(port, () => {
+  console.log(`Server started on port ${port}.`)
+})
